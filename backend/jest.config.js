07b154a94288ceab7111b79f8671fcd298640aef
Olumide Koyenikan <olumide.koyenikan@yahoo.com>
2026-02-23 13:50:25 +0100

    duplicate jest config cleared for jenkinsfile error, while for gitaction error cache dependecy directory added

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  testEnvironment: "node",
-  testMatch: ["**/test/**/*.test.js"]
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: '.',
+      outputName: 'junit.xml'
+    }]
+  ]
 };
